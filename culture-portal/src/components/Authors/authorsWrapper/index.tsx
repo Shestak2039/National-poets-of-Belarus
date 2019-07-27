01751ddb61298310bf3e3d3259bd0f386749404d
m1kash <yarik.tobolich@gmail.com>
2019-07-28 00:50:16 +0300

    feat: add integration contentful in App

--- culture-portal/src/components/Authors/authorsWrapper/index.tsx
+++ culture-portal/src/components/Authors/authorsWrapper/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import SearchForm from './searchForm/SearchForm';
 import AuthorsList from './AuthorsList/AuthorList';
 
-const AuthorsWrapper = () => (
+type AuthorsProps = {authors: any };
+const AuthorsWrapper = ( { authors } : AuthorsProps ) => (
   <>
     <SearchForm />
-    <AuthorsList />
+    <AuthorsList authors={authors} />
   </>
 );
+
+AuthorsWrapper.propTypes = {
+  authors: PropTypes.instanceOf(Array).isRequired
+};
 
 export default AuthorsWrapper;
