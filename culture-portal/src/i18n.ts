d9aa7f8d49f9d244da233dc9dd6b90ec74be2dcd
m1kash <yarik.tobolich@gmail.com>
2019-07-28 15:56:42 +0300

    feat: add save missing

--- culture-portal/src/i18n.ts
+++ culture-portal/src/i18n.ts
@@ -11,9 +11,9 @@ i18n
     backend: {
       projectId: '60788b14-bdc1-4a02-b8c8-3fd486c271c4',
       apiKey: '09e284b4-2e74-480d-a5ab-eba7cac3f5f4',
-      referenceLng: 'en',
-      saveMissing: true
+      referenceLng: 'en'
     },
+    saveMissing: true,
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false
