61a2fc437719bc0ff42cc5b3da835575079a94d3
m1kash <yarik.tobolich@gmail.com>
2019-07-28 13:22:09 +0300

    feat: add translate phrases

--- culture-portal/src/components/Authors/authorsWrapper/AuthorsList/AuthorList.tsx
+++ culture-portal/src/components/Authors/authorsWrapper/AuthorsList/AuthorList.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import './author-list.css';
 
 const AuthorList = ({ authors = [] } : {authors: any }) => {
+  const { t } = useTranslation();
+
   return (
     authors.map((author: any) => (
           <li key={author.fields.slug} className="authors-list__item">
-            <Link to={`/authors/${author.fields.slug}`}>{author.fields.nameAuthor}</Link>
+            <Link to={`/authors/${author.fields.slug}`}>{t(author.fields.nameAuthor)}</Link>
           </li>
       )
     )
