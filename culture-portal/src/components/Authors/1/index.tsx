15588b88da8436f1f81248b6b85c761a2191bab9
ksandrsis <ksandrsis@gmail.com>
2019-07-28 12:54:13 +0300

    feat: add example of Galery Overlay

--- culture-portal/src/components/Authors/1/index.tsx
+++ culture-portal/src/components/Authors/1/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import PropTypes  from 'prop-types';
+import PropTypes from 'prop-types';
 
-const Author = ({ data } : { data: any }) => (
+const Author = ({ data }: { data: any }) => (
 
   <div>Some info about author {data.nameAuthor}</div>
 );
