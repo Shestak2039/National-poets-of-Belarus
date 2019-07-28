02c701e58a5cab478b714704b1ba6dcc763922f0
Anton <zhl96@mail.ru>
2019-07-28 22:26:28 +0300

    feat: add material-ui search-bar

--- culture-portal/src/components/Authors/authorsWrapper/searchForm/SearchForm.tsx
+++ culture-portal/src/components/Authors/authorsWrapper/searchForm/SearchForm.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import clsx from 'clsx';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 
 import './search-form.css';
 
-const SearchForm = () => (
-  <>
-    <input placeholder="Find author..." />
-  </>
-);
+const SearchForm = ()  => {
+  const useStyles = makeStyles(theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200,
+    },
+    dense: {
+      marginTop: 19,
+    },
+    menu: {
+      width: 200,
+    },
+  }));
+
+  const classes = useStyles();
+    return (
+    <form className={classes.container} noValidate autoComplete="off">
+      <TextField
+        id="standard-search"
+        label="S field"
+        type="search"
+        className={classes.textField}
+        margin="normal"
+      />
+    </form>
+    );
+}
 
 export default SearchForm;
