a19cae9f4263f6ef7c4b66e77771593edcf9e1fc
Anton <zhl96@mail.ru>
2019-07-29 20:58:59 +0300

    feat: add test handler and some style to search

--- culture-portal/src/components/Authors/authorsWrapper/searchForm/SearchForm.tsx
+++ culture-portal/src/components/Authors/authorsWrapper/searchForm/SearchForm.tsx
@@ -1,42 +1,25 @@
 import React, { Component } from 'react';
-import clsx from 'clsx';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
+
 
 import './search-form.css';
 
-const SearchForm = ()  => {
-  const useStyles = makeStyles(theme => ({
-    container: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: 200,
-    },
-    dense: {
-      marginTop: 19,
-    },
-    menu: {
-      width: 200,
-    },
-  }));
+const SearchForm = () => {
 
-  const classes = useStyles();
-    return (
-    <form className={classes.container} noValidate autoComplete="off">
-      <TextField
-        id="standard-search"
-        label="S field"
-        type="search"
-        className={classes.textField}
-        margin="normal"
-      />
-    </form>
-    );
+  const handleChangle = (e: any) => {
+    console.log(e.target.value);
+  }
+  return (
+    <TextField
+      id="standard-search"
+      type="search"
+      margin="normal"
+      style={{ margin: 16 }}
+      placeholder='a'
+      fullWidth
+      onChange={handleChangle}
+  />
+  );
 }
 
 export default SearchForm;
