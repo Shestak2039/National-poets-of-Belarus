716b69e4ffa8b47c82892ea9290089e1cc87fbf3
Tanya92 <tatiana_mironchik@tut.by>
2019-07-30 15:02:28 +0300

    refactor: change footer styles
    refactor: change footer styles

--- culture-portal/src/components/footer/index.tsx
+++ culture-portal/src/components/footer/index.tsx
@@ -1,13 +1,47 @@
-import './footer.css';
-import * as React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography/Typography";
 
-export default class Footer extends React.Component {
+const useStyles = makeStyles(() => ({
+    footerBlock: {
+      position: "fixed",
+      left: "0",
+      bottom: "0",
+      width: '100%',
+      height: '60px',
+      background: '#212121',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    phantom: {
+      display: 'block',
+      padding: '20px 0',
+      height: '60px',
+      width: '100%',
+    },
+    text: {
+      color: '#FFFFFF',
+      letterSpacing: '1.25px',
+      textTransform: 'uppercase',
+      fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      fontWeight: 'bold',
+      fontSize: '16px',
+      lineHeight: '18px',
+    }
+}));
 
-  render() {
+export default function Footer(): JSX.Element {
+
+   const classes = useStyles();
+
     return (
-      <footer className="footer-container">
-        Copyright 2019 - RSSchool Group 9
+      <footer>
+        <div className={classes.phantom} />
+        <div className={classes.footerBlock}>
+          <Typography component="p" className={classes.text}> Copyright 2019 - RSSchool Group 9.</Typography>
+        </div>
       </footer>
     );
-  }
-}
+};
