795fcb62d6f408ce9791d536ab81e9b4eed911ca
alexgrist14 <agurec.fasasf@gmail.com>
2019-07-30 21:07:29 +0300

    feat: cahnge header style

--- culture-portal/src/components/header/Navigation/NavigationUI.tsx
+++ culture-portal/src/components/header/Navigation/NavigationUI.tsx
@@ -1,12 +1,10 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { Switch, Route, NavLink, BrowserRouter } from "react-router-dom";
-import AuthorsWrapper from '../../Authors/authorsWrapper/index';
 
 interface TabPanelProps {
 
@@ -45,6 +43,18 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
       backgroundColor: theme.palette.background.paper,
     },
+    headerMenu: {
+      flexDirection: 'row',
+      backgroundColor: 'white',
+      color: 'black',
+      boxShadow: 'none',
+      '@media screen and (max-width: 450px)': {
+        flexDirection: 'column'
+      }
+    },
+    button: {
+      backgroundColor: 'none',
+    },
   }),
 );
 
@@ -56,47 +66,14 @@ export default function SimpleTabs() {
     setValue(newValue);
   }
 
-
-
-  //   return (
-  //     <BrowserRouter>
-  //       <div className="App">
-  //         <Route
-  //           path="/"
-  //           render={({ location }) => (
-  //             <Fragment>
-  //               <Tabs value={location.pathname}>
-  //                 <Tab label="Item One" component={NavLink} to="/" />
-  //                 <Tab label="Item Two" component={NavLink} to="/authors/" />
-  //                 <Tab
-  //                   label="Item Three"
-  //                   href="#basic-tabs"
-  //                   component={NavLink}
-  //                   to="/about-us/"
-  //                 />
-  //               </Tabs>
-  //               <Switch>
-  //                 <Route exact path="/authors" component={AuthorsWrapper} />
-  //                 <Route path="/tab3" render={() => <div>Tab 3</div>} />
-  //                 <Route path="/" render={() => <div>Tab 1</div>} />
-  //               </Switch>
-  //             </Fragment>
-  //           )}
-  //         />
-  //       </div>
-  //     </BrowserRouter>
-  //   );
-  // }
   return (
     <nav className="nav">
       <ul className="navigation">
         <div className={classes.root}>
-          <AppBar position="static">
-            <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-              <li className="navigation__link"><NavLink to="/" activeClassName='active'><Tab label="Main" {...a11yProps(0)} /></NavLink></li>
-              <li className="navigation__link"><NavLink to="/authors/" activeClassName='active'><Tab label="Authors" {...a11yProps(1)} /></NavLink></li>
-              <li className="navigation__link"><NavLink to="/about-us/" activeClassName='active'><Tab label="About us" {...a11yProps(2)} /></NavLink></li>
-            </Tabs>
+          <AppBar className={classes.headerMenu} position="static">
+            <li className="navigation__link"><NavLink to="/" activeClassName='active'><Tab className={classes.button} label="Main" {...a11yProps(0)} /></NavLink></li>
+            <li className="navigation__link"><NavLink to="/authors/" activeClassName='active'><Tab label="Authors" {...a11yProps(1)} /></NavLink></li>
+            <li className="navigation__link"><NavLink to="/about-us/" activeClassName='active'><Tab label="About us" {...a11yProps(2)} /></NavLink></li>
           </AppBar>
         </div >
       </ul>
