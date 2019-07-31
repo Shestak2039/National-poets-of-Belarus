ef18a583c6b00436fde4f3ee916f5db9704ab299
Tanya92 <tatiana_mironchik@tut.by>
2019-07-31 14:07:14 +0300

    fix: change footer position

--- culture-portal/src/components/footer/index.tsx
+++ culture-portal/src/components/footer/index.tsx
@@ -1,47 +1,41 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from "@material-ui/core/Typography/Typography";
+import Container from "@material-ui/core/Container/Container";
 
-const useStyles = makeStyles(() => ({
-    footerBlock: {
-      position: "fixed",
-      left: "0",
-      bottom: "0",
+const useStyles = makeStyles((theme) => {
+  return ({
+    footer: {
       width: '100%',
       height: '60px',
       background: '#212121',
       display: 'flex',
       alignItems: 'center',
-      justifyContent: 'center',
-    },
-    phantom: {
-      display: 'block',
-      padding: '20px 0',
-      height: '60px',
-      width: '100%',
+      marginTop: 'auto'
     },
     text: {
       color: '#FFFFFF',
       letterSpacing: '1.25px',
       textTransform: 'uppercase',
+      textAlign: 'center',
       fontFamily: 'Roboto',
       fontStyle: 'normal',
       fontWeight: 'bold',
       fontSize: '16px',
       lineHeight: '18px',
     }
-}));
+  });
+});
 
 export default function Footer(): JSX.Element {
 
    const classes = useStyles();
 
     return (
-      <footer>
-        <div className={classes.phantom} />
-        <div className={classes.footerBlock}>
-          <Typography component="p" className={classes.text}> Copyright 2019 - RSSchool Group 9.</Typography>
-        </div>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography  component="p" className={classes.text}>Copyright 2019 - RSSchool Group 9.</Typography>
+        </Container>
       </footer>
     );
 };
