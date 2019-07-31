33245bc8c51de11e072e403773fd3dccaa9a1180
Vladislav Shestakov <vlad.shestackov2013@mail.ru>
2019-07-31 22:42:04 +0300

    refactor: change call about-us component

--- culture-portal/src/components/AboutUs/personFromTeam/PersonFromTeam.tsx
+++ culture-portal/src/components/AboutUs/personFromTeam/PersonFromTeam.tsx
@@ -5,20 +5,16 @@ import { DataProps } from '../../../types/index';
 import clsx from 'clsx';
 import { loadCSS } from 'fg-loadcss';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import { red, blue } from '@material-ui/core/colors';
+import { blue } from '@material-ui/core/colors';
 import Icon from '@material-ui/core/Icon';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
 
 import './person-from-team.css';
-
-import img from './photo_tel.jpg';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -26,15 +22,23 @@ const useStyles = makeStyles((theme: Theme) =>
       maxWidth: 345,
       margin: 40,
       padding: 0,
+      boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.14), 0px 3px 4px rgba(0, 0, 0, 0.12), 0px 1px 5px rgba(0, 0, 0, 0.2)',
+      borderRadius: '4px',
     },
     icon: {
       margin: theme.spacing(2),
       color: blue[800],
     },
     iconHover: {
-      margin: theme.spacing(2),
+      marginRight: theme.spacing(2),
+      marginBottom: theme.spacing(2),
+      marginTop: theme.spacing(2),
+    },
+    linkHover: {
+      color: 'rgba(63, 81, 181, 0.87)',
+      transition: 'all 0.3s linear',
       '&:hover': {
-        color: red[800],
+        color: 'rgba(0, 0, 0, 0.6)',
       },
     },
   }),
@@ -55,34 +59,33 @@ const PersonFromTeam = (props: DataProps) => {
 
   return (
     <Card className={classes.card}>
-      <CardActionArea>
         <CardMedia
           component="img"
           alt="Contemplative Reptile"
           height="350"
-          image={img}
+          image={photo}
           title="Contemplative Reptile"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Nickname
+            {nickname}
           </Typography>
-          <Box display="flex" bgcolor="background.paper">
-            <Box ml={-1.6}>
-              <Icon className={clsx(classes.icon, 'fab fa-github')} />
-            </Box>
-            <Box alignSelf="center">
-              <Typography variant="h6" component="h3">
-                Shestak2039
-              </Typography>
-            </Box>
-          </Box>
+            <Link className={classes.linkHover} href={`https://github.com/${githubAccount}`} underline='none' target='_blank'>
+              <Box display="flex" bgcolor="background.paper">
+                <Box>
+                  <Icon className={clsx(classes.iconHover, 'fab fa-github')} />
+                </Box>
+                <Box alignSelf="center">
+                  <Typography variant="h6" component="h3">
+                    {githubAccount}
+                  </Typography>
+                </Box>
+              </Box>
+            </Link>
           <Typography variant="body2" color="textSecondary" component="p">
-            contribution contribution contribution contribution contribution contribution
-            contribution contribution contribution contribution contribution contribution
+            {contribution}
           </Typography>
         </CardContent>
-      </CardActionArea>
     </Card>
   );
 };
