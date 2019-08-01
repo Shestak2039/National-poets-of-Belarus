6541ac3b8694cdf6c7ceaf9eaa9f0a4fcfad8e47
Vladislav Shestakov <vlad.shestackov2013@mail.ru>
2019-08-01 18:20:10 +0300

    feat: add react-yandex-maps

--- culture-portal/src/components/Authors/1/index.tsx
+++ culture-portal/src/components/Authors/1/index.tsx
@@ -4,10 +4,17 @@ import PropTypes from 'prop-types';
 import Foto from '../../OverlayFoto';
 import Video from '../../OverlayVideo';
 
+import { YMaps, Map, Placemark } from 'react-yandex-maps';
+
 const Author = ({ data }: { data: any }) => {
   console.log(data);
   return (
     <>
+    <YMaps>
+      <Map defaultState={{center: [53.899362, 27.560870], zoom: 17 }} width={'100%'} height={'50vh'}>
+        <Placemark geometry={[53.899362, 27.560870]} />
+      </Map>
+    </YMaps>
       <div> Some info about author {data.nameAuthor}</div>
       <Foto data={data} />
       <Video data={data} />
