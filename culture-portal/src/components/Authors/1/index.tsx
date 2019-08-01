1bfec8774b9f3274f0c318f2d079c15b15f596d9
Vladislav Shestakov <vlad.shestackov2013@mail.ru>
2019-08-01 18:50:52 +0300

    feat: add yandex-map component

--- culture-portal/src/components/Authors/1/index.tsx
+++ culture-portal/src/components/Authors/1/index.tsx
@@ -3,27 +3,22 @@ import PropTypes from 'prop-types';
 
 import Foto from '../../OverlayFoto';
 import Video from '../../OverlayVideo';
-
-import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import YandexMap from '../YandexMap/YandexMap';
 
 const Author = ({ data }: { data: any }) => {
   console.log(data);
   return (
     <>
-    <YMaps>
-      <Map defaultState={{center: [53.899362, 27.560870], zoom: 17 }} width={'100%'} height={'50vh'}>
-        <Placemark geometry={[53.899362, 27.560870]} />
-      </Map>
-    </YMaps>
       <div> Some info about author {data.nameAuthor}</div>
       <Foto data={data} />
       <Video data={data} />
+      <YandexMap coordinates={data.map.coordinates} />
     </>
   )
 };
 
 Author.propTypes = {
-  data: PropTypes.instanceOf(Object).isRequired
+  data: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default Author;
