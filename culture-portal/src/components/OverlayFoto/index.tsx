634c5d297111bbe90ba1ac308ab35317620e8840
m1kash <yarik.tobolich@gmail.com>
2019-08-04 07:45:48 +0300

    feat: add styles gallery

--- culture-portal/src/components/OverlayFoto/index.tsx
+++ culture-portal/src/components/OverlayFoto/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles} from "@material-ui/styles";
 import { BigPictureGallery } from "react-bigpicture";
 
 BigPictureGallery.propTypes.noLoader = PropTypes.bool;
 
+const useStyles = makeStyles({
+  authorsPhoto: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  }
+});
+
+
 const Foto = ({ data }: { data: any }) => {
+  const classes = useStyles();
   const images = data.gallery.map((el: any) => (
     <img
       key={`key${el.fields.file.url}`}
@@ -15,7 +26,7 @@ const Foto = ({ data }: { data: any }) => {
 
   ));
   return (
-    <div className='autorsPhoto'>
+    <div className={classes.authorsPhoto}>
       <BigPictureGallery loop={true}>{images}</BigPictureGallery>
     </div>
   )
