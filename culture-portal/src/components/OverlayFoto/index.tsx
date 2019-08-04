06eeb2e1c45e3bf39a9d7e78cfe51f16c42848f6
ksandrsis <ksandrsis@gmail.com>
2019-08-04 18:36:59 +0300

    fix: fix scroll authars post

--- culture-portal/src/components/OverlayFoto/index.tsx
+++ culture-portal/src/components/OverlayFoto/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles} from "@material-ui/styles";
+import { makeStyles } from "@material-ui/styles";
 import { BigPictureGallery } from "react-bigpicture";
 
 BigPictureGallery.propTypes.noLoader = PropTypes.bool;
